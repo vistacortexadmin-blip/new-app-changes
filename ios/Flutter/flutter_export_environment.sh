3ef4e43c8157340e433f5e6304b0224f0c8ebe89
Borigam Umesh <umeshborigam875@gmail.com>
2026-09-08 21:12:10 +0530

    Update configuration files: standardize FLUTTER_ROOT path and disable Kotlin incremental compilation

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Vistacortex\flutter"
+export "FLUTTER_ROOT=D:\flutter"
 export "FLUTTER_APPLICATION_PATH=D:\Vistacortex"
 export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Vistacortex\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
