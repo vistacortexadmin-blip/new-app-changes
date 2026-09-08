43db05343e2a92a15fda699ab436ec92b24aaaa9
Borigam Umesh <umeshborigam875@gmail.com>
2026-09-08 21:52:23 +0530

    Fix TabBarView crash: remove accidental 4th child (Coming Soon placeholder)

--- lib/features/reminders/views/reminders_screen.dart
+++ lib/features/reminders/views/reminders_screen.dart
@@ -110,7 +110,6 @@ class _RemindersScreenState extends ConsumerState<RemindersScreen>
                 children: [
                   _buildDailyDosesTab(context),
                   _buildRefillSupplyTab(context),
-                  const Center(child: Text("Tests Tab (Coming Soon)")),
                   _buildUpcomingTestsTab(context),
                 ],
               ),
